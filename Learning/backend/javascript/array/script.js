8e97cbc956da23e0fba62e8a3304519e8d247f6b
ks-vivek <ksvivek8872@gmail.com>
2025-11-28 23:34:55 +0530

    Learning Array

---
+++ Learning/backend/javascript/array/script.js
@@ -0,0 +1,45 @@
+// fundamentals of JavaScript
+// arrays and objects
+// functions return
+// async js coding
+// forEach map filter find indexOf
+
+// forEach:-
+// let arr = [1, 2, 3, 4, 5];
+// let ans = arr.forEach(function(val){
+// console.log(val + " hello");
+// })
+
+// map:-
+// let arr = [1, 2, 3, 4, 5];
+
+// let ans = arr.map((val) => {
+//   //return 13;
+//   return val *3;
+// });
+
+// console.log(ans);
+
+// Filter :- 
+// let arr = [1, 2, 3, 4, 5];
+
+// let ans = arr.filter((val)=>{
+//     if(val > 3)return true;
+//     else return false;
+// })
+// console.log(ans);
+
+
+// find:-
+// let arr = [1, 2, 3, 4, 5];
+// let ans = arr.find(function(val){
+//     if(val === 2)return val;
+// })
+// console.log(ans);
+
+
+// indexOf:-
+let arr = [1, 2, 3, 4, 5];
+// let ans = arr.indexOf(5);
+// console.log(ans);
+console.log(arr.indexOf(5));
