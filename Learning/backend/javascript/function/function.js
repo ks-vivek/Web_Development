6b1319b1305a8189c0c98a249b5c9ffdcf96c447
ks-vivek <ksvivek8872@gmail.com>
2025-11-28 23:38:43 +0530

    Function Learning in JavaScript

---
+++ Learning/backend/javascript/function/function.js
@@ -0,0 +1,21 @@
+
+// let arr = [1, 2, 3, 4];
+// console.log(arr.length);
+
+
+// function abcd(a, b){
+
+// console.log('hello'); // hello
+// }
+
+// console.log(abcd.length); // The length of abcd function is 2 because no. of parameter is 2
+
+
+// Function Return:-
+
+function abc(){
+    return 24;
+};
+
+let ans = abc();
+console.log(ans); // this will return 24;
